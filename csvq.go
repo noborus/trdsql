8015376da9e44cd48c829f09e0d78f562f17f3b0
Noboru Saito <noborus@netlab.jp>
2017-07-05 08:40:50 +0900

    Added support file name including ".".
    The SQL table name was changed to the rewritten file name.
    Added test.csv.

--- csvq.go
+++ csvq.go
@@ -14,24 +14,25 @@ import (
 	"github.com/xwb1989/sqlparser"
 )
 
-func csvprint(db *sql.DB, table string, row []string) {
-	columns := make([]string, len(row))
-	for i := 0; i < len(row); i++ {
-		columns[i] = "c" + strconv.Itoa(i+1)
-	}
-	c := strings.Join(columns, ",")
-	sql := "INSERT INTO " + table + " (" + c + ") VALUES ('" + strings.Join(row, "','") + "');"
-	//	fmt.Println(sql)
+func rowimport(db *sql.DB, table string, columns string, row []string) {
+	sql := "INSERT INTO " + table + " (" + columns + ") VALUES ('" + strings.Join(row, "','") + "');"
 	_, err := db.Exec(sql)
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func rowRead(db *sql.DB, reader *csv.Reader, table string, header []string) {
-	if header != nil {
-		csvprint(db, table, header)
+func columnNames(row []string) string {
+	columns := make([]string, len(row))
+	for i := 0; i < len(row); i++ {
+		columns[i] = "c" + strconv.Itoa(i+1)
 	}
+	return strings.Join(columns, ",")
+}
+
+func csvimport(db *sql.DB, reader *csv.Reader, table string, header []string) {
+	columns := columnNames(header)
+	rowimport(db, table, columns, header)
 	for {
 		record, err := reader.Read()
 		if err == io.EOF {
@@ -41,11 +42,11 @@ func rowRead(db *sql.DB, reader *csv.Reader, table string, header []string) {
 				log.Fatal("ERROR:", err)
 			}
 		}
-		csvprint(db, table, record)
+		rowimport(db, table, columns, record)
 	}
 }
 
-func csvRead(db *sql.DB, filename string) (header []string, reader *csv.Reader) {
+func csvRead(filename string) (header []string, reader *csv.Reader) {
 	file, err := os.Open(filename)
 	if err != nil {
 		log.Fatal("ERROR:", err)
@@ -60,9 +61,16 @@ func csvRead(db *sql.DB, filename string) (header []string, reader *csv.Reader) 
 
 func getTable(sqlNode sqlparser.SQLNode) string {
 	table, _ := sqlNode.(*sqlparser.Select)
-	tablename := sqlparser.String(table.From)
-	fmt.Println(tablename)
+	tablename := "dummy"
+	if table != nil {
+		tablename = sqlparser.String(table.From)
+	}
 	return tablename
+}
+
+func prerewrite(sqlstr string, oldname string, newname string) (rewrite string) {
+	rewrite = strings.Replace(sqlstr, oldname, newname, -1)
+	return rewrite
 }
 
 func rewriteTable(tree sqlparser.SQLNode, tablename string) string {
@@ -75,39 +83,39 @@ func rewriteTable(tree sqlparser.SQLNode, tablename string) string {
 		return []byte(s)
 	}
 	sqlparser.Rewrite(tree, rewriter)
-	/*
-		buf := sqlparser.NewTrackedBuffer(nil)
-		buf.Myprintf("%v", tree)
-		pq := buf.ParsedQuery()
-		bytes, _ := pq.GenerateQuery(map[string]interface{}{"id": 1})
-		return string(bytes)
-	*/
 	return sqlparser.String(tree)
 }
 
 func dbconnect() *sql.DB {
 	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR:", err)
 	}
 	return db
 }
 
-func dbcreate(db *sql.DB, table string, header []string) *sql.DB {
+func dbdisconnect(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		fmt.Println("ERROR:", err)
+	}
+}
+
+func dbcreate(db *sql.DB, table string, header []string) {
 	columns := make([]string, len(header))
 	for i := 0; i < len(header); i++ {
 		columns[i] = "c" + strconv.Itoa(i+1) + " text"
 	}
 	c := strings.Join(columns, ",")
-	fmt.Println(c)
+	fmt.Println("CREATE TABLE", table, "(", c, ")")
 	_, err := db.Exec("CREATE TABLE " + table + "(" + c + ")")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR:", err)
 	}
-	return db
 }
 
 func dbselect(db *sql.DB, sqlstr string) {
+	fmt.Println(sqlstr)
 	rows, err := db.Query(sqlstr)
 	if err != nil {
 		fmt.Println(err)
@@ -115,9 +123,8 @@ func dbselect(db *sql.DB, sqlstr string) {
 	defer rows.Close()
 	columns, err := rows.Columns()
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR:", err)
 	}
-	fmt.Printf("rows length:%d\n", len(columns))
 	values := make([]sql.RawBytes, len(columns))
 	results := make([]string, len(columns))
 	scanArgs := make([]interface{}, len(values))
@@ -138,16 +145,20 @@ func dbselect(db *sql.DB, sqlstr string) {
 }
 
 func main() {
-	sqls := os.Args[1]
-	fmt.Println("orgin:", sqls)
-	tree, _ := sqlparser.Parse(sqls)
-	table := getTable(tree)
-	fmt.Println("tablename:", table)
-	rewrite := rewriteTable(tree, table)
-	fmt.Println("rewrite:", rewrite)
+	sqlstr := os.Args[1]
+	fmt.Println("sql:", sqlstr)
+	tree, _ := sqlparser.Parse(sqlstr)
+	tablename := getTable(tree)
+	rtable := strings.Replace(tablename, ".", "_", -1)
+	fmt.Println("rewrite:", tablename, rtable)
+	sqlr := prerewrite(sqlstr, tablename, rtable)
+	fmt.Println("sql:", sqlr)
+
+	header, reader := csvRead(tablename)
+
 	db := dbconnect()
-	header, reader := csvRead(db, table)
-	db = dbcreate(db, "_", header)
-	rowRead(db, reader, "_", header)
-	dbselect(db, rewrite)
+	dbcreate(db, rtable, header)
+	csvimport(db, reader, rtable, header)
+	dbselect(db, sqlr)
+	dbdisconnect(db)
 }
