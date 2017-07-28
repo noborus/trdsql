57fe342814541e8d394aef0af9714934c4a580c7
Noboru Saito <noborus@netlab.jp>
2017-07-28 21:18:54 +0900

    Fix darwin's zip name.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
 TARGET="dist/trdsql_{{.OS}}_{{.Arch}}/{{.Dir}}"
-gox -cgo -os "linux" -arch "386 amd64" -output ${TARGET}
 
-CC=x86_64-w64-mingw32-gcc gox -cgo -os "windows" -arch "amd64" -output ${TARGET}
-CC=i686-w64-mingw32-gcc gox -cgo -os "windows" -arch "386" -output ${TARGET}
+if [ "$TRAVIS_OS_NAME" == "linux" ]; then
+    gox -cgo -os "linux" -arch "386 amd64" -output ${TARGET}
+    CC=x86_64-w64-mingw32-gcc gox -cgo -os "windows" -arch "amd64" -output ${TARGET}
+    CC=i686-w64-mingw32-gcc gox -cgo -os "windows" -arch "386" -output ${TARGET}
+elif [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    gox -cgo -os "darwin" -arch "386 amd64" -output ${TARGET}
+fi
