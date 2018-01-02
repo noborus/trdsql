3667f07b86e44d81545f36e7bbb06c389114f7be
Noboru Saito <noborus@netlab.jp>
2018-01-02 16:04:48 +0900

    remove

--- ltsv_test.go
+++ ltsv_test.go
@@ -1,24 +1,8 @@
 package main
 
 import (
-	"strings"
 	"testing"
-
-	"github.com/najeira/ltsv"
 )
-
-func TestLtsvRead(t *testing.T) {
-	const ltsvStream = `
-ID:1	name:testa
-ID:2	name:testb
-	`
-	s := strings.NewReader(ltsvStream)
-	reader := ltsv.NewReader(s)
-	r, _ := reader.Read()
-	if r["ID"] != "1" || r["name"] != "testa" {
-		t.Error("invalid value", r["ID"])
-	}
-}
 
 func TestLtsvInputNew(t *testing.T) {
 	trdsql := trdsqlNew()
