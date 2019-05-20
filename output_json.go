7e654ad58bf92ea8a70f6fb61a9e93a980ec85a2
Noboru Saito <noborusai@gmail.com>
2019-05-20 16:31:47 +0900

    Apply json type

--- output_json.go
+++ output_json.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"unicode/utf8"
 )
 
 // JSONOut provides methods of the Output interface
 type JSONOut struct {
 	writer  *json.Encoder
-	results []map[string]string
+	results []map[string]interface{}
 }
 
 func (trdsql *TRDSQL) jsonOutNew() Output {
@@ -19,18 +21,31 @@ func (trdsql *TRDSQL) jsonOutNew() Output {
 
 // First is preparation
 func (js *JSONOut) First(columns []string, types []string) error {
-	js.results = make([]map[string]string, 0)
+	js.results = make([]map[string]interface{}, 0)
 	return nil
 }
 
 // RowWrite is Addition to array
 func (js *JSONOut) RowWrite(values []interface{}, columns []string) error {
-	m := make(map[string]string, len(columns))
+	m := make(map[string]interface{}, len(columns))
 	for i, col := range values {
-		m[columns[i]] = valString(col)
+		m[columns[i]] = valInterface(col)
 	}
 	js.results = append(js.results, m)
 	return nil
+}
+
+func valInterface(v interface{}) interface{} {
+	switch t := v.(type) {
+	case []byte:
+		if ok := utf8.Valid(t); ok {
+			return string(t)
+		} else {
+			return `\x` + hex.EncodeToString(t)
+		}
+	default:
+		return v
+	}
 }
 
 // Last is Actual output
