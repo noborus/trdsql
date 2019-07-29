3e574f05a3f5945bba70db4ceed2883e3babf2d1
Noboru Saito <noborusai@gmail.com>
2019-07-29 11:43:03 +0900

    Added example of CSV to output all quoted

---
+++ _example/quotecsv/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/noborus/trdsql"
+)
+
+type QuoteWrite struct {
+	writer io.Writer
+}
+
+func NewQuoteCSVWriter(w io.Writer) *QuoteWrite {
+	return &QuoteWrite{
+		writer: w,
+	}
+}
+
+func (w *QuoteWrite) PreWrite(columns []string, types []string) error {
+	return nil
+}
+
+func (w *QuoteWrite) WriteRow(values []interface{}, columns []string) error {
+	qColumns := make([]string, len(values))
+	for i, col := range values {
+		qColumns[i] = strconv.Quote(trdsql.ValString(col))
+	}
+	_, err := w.writer.Write([]byte(strings.Join(qColumns, ",") + "\n"))
+	return err
+}
+func (w *QuoteWrite) PostWrite() error {
+	return nil
+}
+
+func main() {
+	trd := trdsql.NewTRDSQL(
+		trdsql.NewImporter(),
+		trdsql.NewExporter(NewQuoteCSVWriter(os.Stdout)),
+	)
+	err := trd.Exec("SELECT * FROM testdata/test.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module github.com/noborus/trdsql
 
 require (
 	github.com/go-sql-driver/mysql v1.4.1
-	github.com/lib/pq v1.1.1
+	github.com/lib/pq v1.2.0
 	github.com/mattn/go-runewidth v0.0.4
-	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/mattn/go-sqlite3 v1.11.0
 	github.com/noborus/tbln v0.0.1
 	github.com/olekukonko/tablewriter v0.0.1
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
-	golang.org/x/sys v0.0.0-20190626221950-04f50cda93cb // indirect
+	golang.org/x/sys v0.0.0-20190726091711-fc99dfbffb4e // indirect
 )
