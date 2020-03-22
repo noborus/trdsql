14463055b5312fa47f95303d46de887e511f9e2e
Noboru Saito <noborusai@gmail.com>
2020-03-22 11:44:36 +0900

    Modified tab completion notation according to trdsql command.

--- completion/trdsql-completion.zsh
+++ completion/trdsql-completion.zsh
@@ -5,39 +5,39 @@ function _trdsql {
   _arguments -C \
     '-help[display usage information.]' \
     '-version[display version information.]' \
-    '-config[Configuration file location.]:file:_files' \
-    '-db[Specify db name of the setting.]:db specify:__trdsql_db' \
+    '-config[configuration file location.]:file:_files' \
+    '-db[specify db name of the setting.]:db specify:__trdsql_db' \
     '-dblist[display db information.]' \
     '-debug[debug print.]' \
     '-driver[database driver.]:driver specify:__trdsql_driver' \
     '-dsn[database connection option.]' \
-    '-id[Field delimiter for input. (default ",")]::' \
-    '-ih[The first line is interpreted as column names.]' \
-    '(-icsv -iltsv -ijson -itbln)-ig[Guess format from extension.]' \
+    '-id[field delimiter for input. (default ",")]::' \
+    '-ih[the first line is interpreted as column names.]' \
+    '(-icsv -iltsv -ijson -itbln)-ig[guess format from extension.]' \
     '(-ig -iltsv -ijson -itbln)-icsv[CSV format for input.]' \
     '(-ig -icsv -ijson -itbln)-iltsv[LTSV format for input.]' \
     '(-ig -icsv -iltsv -itbln)-ijson[JSON format for input.]' \
     '(-ig -icsv -iltsv -ijson)-itbln[TBLN format for input.]' \
-    '-is[Skip header row.]::' \
+    '-is[skip header row.]::' \
     '-ir[umber of row preread for column determination.(default 1)]::' \
-    '-out[Output file name.]' \
-    '-out-without-guess[Output without guessing from file name.]' \
-    '-od[Field delimiter for output. (default ",")]' \
-    '-oq[Quote character for output. (default "\"")]' \
-    '-oaq[Enclose all fields in quotes for output.]' \
-    '-ocrlf[Use CRLF for output.]' \
-    '-oh[Output column name as header.]' \
+    '-out[output file name.]' \
+    '-out-without-guess[output without guessing (when using -out).]' \
+    '-od[field delimiter for output. (default ",")]' \
+    '-oq[quote character for output. (default "\"")]' \
+    '-oaq[enclose all fields in quotes for output.]' \
+    '-ocrlf[use CRLF for output. End each output line with '\\r\\n' instead of '\\n'.]' \
+    '-oh[output column name as header.]' \
     '-oat[ASCII Table format for output.]' \
     '-ojson[JSON format for output.]' \
     '-ojsonl[JSONL format for output.]' \
     '-oltsv[LTSV format for output.]' \
-    '-omd[Mark Down format for output.]' \
+    '-omd[Markdown format for output.]' \
     '-oraw[Raw format for output.]' \
     '-ovf[Vertical format for output.]' \
-    '-oz[Compression format.]:compression:_values "" "gz" "bz2" "zst" "lz4" "xz"' \
-    '-a[Analyze file and suggest SQL.]:file:_files' \
-    '-A[Analyze but only suggest SQL.]:file:_files' \
-    '-q[Read query from the provided filename.]:file:_files -g "*.(SQL|sql)"' \
+    '-oz[output compression format.]:compression:_values "" "gz" "bz2" "zst" "lz4" "xz"' \
+    '-a[analyze the file and suggest SQL.]:file:_files' \
+    '-A[analyze the file but only suggest SQL.]:file:_files' \
+    '-q[read query from the provided filename.]:file:_files -g "*.(SQL|sql)"' \
     '1: :__trdsql_sql' \
     '*:file:_files -g "*"'
 }
