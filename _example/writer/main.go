79fe5c1cdcdfa4b61fd1c280e7a61301b3928118
Noboru Saito <noborusai@gmail.com>
2020-05-09 11:39:06 +0900

    Added customized writer use case

---
+++ _example/writer/main.go
@@ -0,0 +1,57 @@
+// writer is an example of using a customized writer.
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/noborus/trdsql"
+)
+
+type Write struct {
+	writer io.Writer
+}
+
+func NewWriter(f io.Writer) *Write {
+	return &Write{
+		writer: f,
+	}
+}
+
+func (w *Write) PreWrite(columns []string, types []string) error {
+	return nil
+}
+
+func (w *Write) WriteRow(values []interface{}, columns []string) error {
+	var err error
+	for i, v := range values {
+		if i > 0 {
+			_, err = w.writer.Write([]byte(","))
+			if err != nil {
+				return err
+			}
+		}
+		_, err = w.writer.Write([]byte(trdsql.ValString(v)))
+		if err != nil {
+			return err
+		}
+	}
+	_, err = w.writer.Write([]byte("\n"))
+	return err
+}
+
+func (w *Write) PostWrite() error {
+	return nil
+}
+
+func main() {
+	trd := trdsql.NewTRDSQL(
+		trdsql.NewImporter(),
+		trdsql.NewExporter(NewWriter(os.Stdout)),
+	)
+	err := trd.Exec("SELECT * FROM input.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -4,17 +4,18 @@ require (
 	github.com/dsnet/compress v0.0.1
 	github.com/frankban/quicktest v1.7.2 // indirect
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/klauspost/compress v1.10.4
-	github.com/lib/pq v1.3.0
+	github.com/klauspost/compress v1.10.5
+	github.com/lib/pq v1.5.2
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
 	github.com/mattn/go-runewidth v0.0.9
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible
 	github.com/noborus/tbln v0.0.1
 	github.com/olekukonko/tablewriter v0.0.4
-	github.com/pierrec/lz4 v2.5.0+incompatible
+	github.com/pierrec/lz4 v2.5.2+incompatible
+	github.com/prometheus/common v0.9.1
 	github.com/ulikunitz/xz v0.5.7
-	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71
-	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
+	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
+	golang.org/x/sys v0.0.0-20200508214444-3aab700007d7 // indirect
 )
 
 go 1.13
