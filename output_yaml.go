6fe5335106d619fb8b853a9d4419ca29987fef24
Noboru Saito <noborusai@gmail.com>
2023-08-20 14:01:51 +0900

    Output with yaml when data is yaml
    If it is a yaml string, encode the yaml string and then output.

--- output_yaml.go
+++ output_yaml.go
@@ -1,19 +1,28 @@
 package trdsql
 
 import (
+	"bytes"
+	"encoding/hex"
+	"strings"
+	"unicode/utf8"
+
 	"github.com/goccy/go-yaml"
 )
 
 // YAMLWriter provides methods of the Writer interface.
 type YAMLWriter struct {
-	writer  *yaml.Encoder
-	results []yaml.MapSlice
+	writer   *yaml.Encoder
+	outNULL  string
+	results  []yaml.MapSlice
+	needNULL bool
 }
 
 // NewYAMLWriter returns YAMLWriter.
 func NewYAMLWriter(writeOpts *WriteOpts) *YAMLWriter {
 	w := &YAMLWriter{}
 	w.writer = yaml.NewEncoder(writeOpts.OutStream)
+	w.needNULL = writeOpts.OutNeedNULL
+	w.outNULL = writeOpts.OutNULL
 	return w
 }
 
@@ -28,10 +37,41 @@ func (w *YAMLWriter) WriteRow(values []interface{}, columns []string) error {
 	m := make(yaml.MapSlice, len(values))
 	for i, col := range values {
 		m[i].Key = columns[i]
-		m[i].Value = col
+		m[i].Value = compatibleYAML(col, w.needNULL, w.outNULL)
 	}
 	w.results = append(w.results, m)
 	return nil
+}
+
+// CompatibleYAML converts the value to a YAML-compatible value.
+func compatibleYAML(v interface{}, needNULL bool, outNULL string) interface{} {
+	var yl interface{}
+	switch t := v.(type) {
+	case []byte:
+		if !bytes.Contains(t, []byte("\n")) {
+			return v
+		}
+		if err := yaml.Unmarshal(t, &yl); err == nil {
+			return yl
+		}
+		if ok := utf8.Valid(t); ok {
+			return string(t)
+		}
+		return `\x` + hex.EncodeToString(t)
+	case string:
+		if !strings.Contains(t, "\n") {
+			return v
+		}
+		if err := yaml.Unmarshal([]byte(t), &yl); err == nil {
+			return yl
+		}
+		return v
+	default:
+		if needNULL {
+			return outNULL
+		}
+		return v
+	}
 }
 
 // PostWrite is Actual output.
