82683ec4b499bd7a245ebae59e22992499175e4a
Noboru Saito <noborusai@gmail.com>
2023-09-23 15:02:42 +0900

    Preventing panic due to invalid yaml file
    Prevents panic, but just displays an error instead of panic.
    This is for issue https://github.com/goccy/go-yaml/issues/373,
    so it may be removed if go-yaml is updated.

--- input_yaml.go
+++ input_yaml.go
@@ -38,7 +38,9 @@ func NewYAMLReader(reader io.Reader, opts *ReadOpts) (*YAMLReader, error) {
 	r.reader = yaml.NewDecoder(reader)
 	r.already = make(map[string]bool)
 
-	r.yamlParse(opts)
+	if err := r.yamlParse(opts); err != nil {
+		return nil, err
+	}
 
 	return r, nil
 }
@@ -56,6 +58,17 @@ func jqParse(q string) (*gojq.Query, error) {
 	return query, nil
 }
 
+func (r *YAMLReader) wrapDecode(v interface{}) (err error) {
+	defer func() {
+		if rec := recover(); rec != nil {
+			err = fmt.Errorf("%s", rec)
+		}
+	}()
+
+	err = r.reader.Decode(v)
+	return
+}
+
 // yamlParse parses YAML and stores it in preRead.
 func (r *YAMLReader) yamlParse(opts *ReadOpts) error {
 	r.limitRead = opts.InLimitRead
@@ -64,9 +77,9 @@ func (r *YAMLReader) yamlParse(opts *ReadOpts) error {
 
 	var top interface{}
 	for i := 0; i < opts.InPreRead; i++ {
-		if err := r.reader.Decode(&top); err != nil {
+		if err := r.wrapDecode(&top); err != nil {
 			if !errors.Is(err, io.EOF) {
-				return err
+				return fmt.Errorf("%w: %w", ErrInvalidYAML, err)
 			}
 			debug.Printf(err.Error())
 			return nil
@@ -207,8 +220,8 @@ func (r *YAMLReader) ReadRow(row []interface{}) ([]interface{}, error) {
 	return v, nil
 }
 
-func (r *YAMLReader) rowParse(row []interface{}, YAMLRow interface{}) []interface{} {
-	switch m := YAMLRow.(type) {
+func (r *YAMLReader) rowParse(row []interface{}, yamlRow interface{}) []interface{} {
+	switch m := yamlRow.(type) {
 	case map[string]interface{}:
 		for i := range r.names {
 			row[i] = r.toString(m[r.names[i]])
@@ -217,7 +230,7 @@ func (r *YAMLReader) rowParse(row []interface{}, YAMLRow interface{}) []interfac
 		for i := range r.names {
 			row[i] = nil
 		}
-		row[0] = r.toString(YAMLRow)
+		row[0] = r.toString(yamlRow)
 	}
 	return row
 }
